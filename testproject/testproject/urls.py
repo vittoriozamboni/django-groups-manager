3de4665adae5f289fa896aa211ec32f72d956342
Oskar Persson <oskar.persson@polken.se>
2017-10-29 13:53:07 +0000

    Remove use of deprecated patterns function

--- testproject/testproject/urls.py
+++ testproject/testproject/urls.py
@@ -1,11 +1,11 @@
 from django.conf import settings
-from django.conf.urls import patterns, include, url
+from django.conf.urls import include, url
 from django.conf.urls.static import static
 from django.contrib import admin
 
 from testproject import views
 
-urlpatterns = patterns('',
+urlpatterns = [
     # Examples:
     # url(r'^$', 'testproject.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
@@ -13,4 +13,4 @@ urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
     url(r'^$', views.TestView.as_view(), name='home'),
     url(r'^groups-manager/', include('groups_manager.urls', namespace='groups_manager')),
-) + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
